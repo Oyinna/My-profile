09ed327cfd5b989b4ed38cada6b3a563832c74c0
Chinyelu Ibute <chinyeluibute@gmail.com>
2020-01-06 12:15:11 +0100

    modified skills

--- resources/views/pages/skill.blade.php
+++ resources/views/pages/skill.blade.php
@@ -10,56 +10,56 @@
             <div class="col-sm-6">
                     <p>PHP</p> 
                     <div class="progress" style="height:15px">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:92%">
                             92%
                         </div>
                     </div>
     
                     <p>Javascript / Jquery</p> 
                     <div class="progress" style="height:15px">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:60%">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:89%">
                             89%
                         </div>
                     </div>
     
                     <p>HTML</p> 
                     <div class="progress" style="height:15px">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:90%">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:95%">
                             95%
                         </div>
                     </div>
     
                     <p>CSS</p> 
                     <div class="progress" style="height:15px">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:94%">
                             94%
                         </div>
                     </div>
 
                     <p>Laravel</p> 
                     <div class="progress" style="height:15px">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:92%">
                             92%
                         </div>
                     </div>
 
                     <p>React / ES6</p> 
                     <div class="progress" style="height:15px">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:90%">
                             90%
                         </div>
                     </div>
 
                     <p> Python</p> 
                     <div class="progress" style="height:15px">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:70%">
                             70%
                         </div>
                     </div>
 
                     <p>MySQL</p> 
                     <div class="progress" style="height:15px">
-                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:95%">
                             95%
                         </div>
                     </div>
